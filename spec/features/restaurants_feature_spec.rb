ec49a69d8fec43f493876652c80570423fe98d97
Tony Young <ty_training@hotmail.co.uk>
2016-01-18 14:58:08 +0000

    created restaurant controller and view

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -8,4 +8,16 @@ feature 'restaurants' do
       expect(page).to have_link 'Add Restaurant'
     end
   end
+
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'Pizza Hut')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content 'Pizza Hut'
+      expect(page).not_to have_content 'No restaurants to see here'
+    end
+  end
 end
