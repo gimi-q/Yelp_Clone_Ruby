a6beb8daf0f7b97254c4adeea18e4b0a5518ea33
Tony Young <ty_training@hotmail.co.uk>
2016-01-19 10:54:15 +0000

    associated restaurants with reviews

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -68,4 +68,20 @@ feature 'restaurants' do
       expect(page).to have_content 'KFC deleted successfully'
     end
   end
+
+end
+
+feature 'reviewing' do
+  before {Restaurant.create name: 'KFC'}
+
+  scenario 'allows users to leave a review using a form' do
+    visit '/restaurants'
+    click_link 'Review KFC'
+    fill_in "Thoughts", with: "so so"
+    select '3', from: 'Rating'
+    click_button 'Leave Review'
+
+    expect(current_path).to eq '/restaurants'
+    expect(page).to have_content('so so')
+  end
 end
