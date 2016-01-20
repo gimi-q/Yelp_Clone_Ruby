eb0c7519b13cc709fdb6ba5d3130d92a3b732fa5
Tony Young <ty_training@hotmail.co.uk>
2016-01-20 15:45:54 +0000

    Added .env for sensitive data

---
+++ spec/models/restaurant_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Restaurant, type: :model do
+  it { is_expected.to have_many :reviews }
+
+  it { is_expected.to have_many(:reviews).dependent(:destroy) }
+
+  it 'is not valid with a name less tha three characters' do
+    restaurant = Restaurant.new(name: 'KF')
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
+
+  it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
+end
