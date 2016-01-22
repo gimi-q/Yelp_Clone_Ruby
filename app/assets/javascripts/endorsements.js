518955ef116e97be6431b17e9d048dfa4d765d33
vegim qehaja <med.batt.uk@gmail.com>
2016-01-22 18:00:04 +0000

    adds working endorsement functionality with passing test

--- app/assets/javascripts/endorsements.js
+++ app/assets/javascripts/endorsements.js
@@ -1,6 +1,4 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+
 $(document).ready(function() {
 
   $('.endorsements-link').on('click', function(event){
@@ -10,6 +8,6 @@ $(document).ready(function() {
 
       $.post(this.href, function(response){
         endorsementCount.text(response.new_endorsement_count);
-    })
-  })
-})
+    });
+  });
+});
